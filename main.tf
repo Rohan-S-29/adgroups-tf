a56a2992fa71d758cc75bd4bcc9f967366c1e2c3
Rohan-S-29 <rohansarabu@gmail.com>
2025-04-04 15:16:34 +0530

    configured permissions to create users

--- main.tf
+++ main.tf
@@ -5,6 +5,12 @@ provider "azurerm" {
   subscription_id = "d8d6ad8c-f635-407e-81b9-4ce76f2d5a10"
 }
 
+# Create Resource group
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
+
 # Create Azure AD Users
 resource "azuread_user" "owner" {
   user_principal_name    = "user1@example.com"
@@ -54,18 +60,18 @@ resource "azuread_group" "devops_team" {
 # Azure SQL Server
 resource "azurerm_mssql_server" "sql_server" {
   name                         = "my-sql-server-123"
-  resource_group_name          = "my-rg"
-  location                     = "East US"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
   administrator_login          = "sqladmin"
-  administrator_login_password = "Password@123"
+  administrator_login_password = var.admin_password
 }
 
 # Azure Storage Account
 resource "azurerm_storage_account" "storage" {
   name                     = "mystorageaccount123"
-  resource_group_name      = "my-rg"
-  location                 = "East US"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
